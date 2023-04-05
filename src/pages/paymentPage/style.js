1c739cfbb350b7a192b0e8fe2bcfdc65a1711f3d
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-04-06 00:57:04 +0900

    결제완료 페이지 구현

--- src/pages/paymentPage/style.js
+++ src/pages/paymentPage/style.js
@@ -18,4 +18,35 @@ export const WhiteBox = styled.div`
   background: white;
   border-radius: 2px;
   border-radius: 7px;
+
+  .imgContent {
+    margin-bottom: 80px;
+  }
+
+  .paymentContent {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  .order {
+    margin-bottom: 3px;
+    font-size: 22px;
+    font-weight: bolder;
+  }
+
+  .account {
+    font-size: 11px;
+    color: #999999;
+    font-weight: 500;
+  }
+`;
+
+export const iceImg = styled.img`
+    width: :130px;
+    height: 130px;
+
 `;

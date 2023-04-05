8075c33e5fb5a9509d328734c16bc6b280b02952
이태랑 <106226701+TaerangLee@users.noreply.github.com>
2023-04-05 21:21:56 +0900

    update the payment code

---
+++ src/pages/paymentPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./style";
+
+function Payment() {
+  return (
+    <S.backCenter>
+      <S.WhiteBox>asd</S.WhiteBox>
+    </S.backCenter>
+  );
+}
+
+export default Payment;
